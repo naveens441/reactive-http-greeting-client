f1c2b6c93e71a78bc86fdf0f1ea7f2b1112b2b57
Naveen Kumar Singh <naveen_kumar_singh@epam.com>
2022-01-04 12:57:29 +0530

    Initial commit

---
+++ src/main/java/reactivehttpgreetingclient/httpgreetingclient/HttpGreetingClientApplication.java
@@ -0,0 +1,13 @@
+package reactivehttpgreetingclient.httpgreetingclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpGreetingClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpGreetingClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reactivehttpgreetingclient/httpgreetingclient/HttpGreetingClientApplicationTests.java
@@ -0,0 +1,13 @@
+package reactivehttpgreetingclient.httpgreetingclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpGreetingClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
